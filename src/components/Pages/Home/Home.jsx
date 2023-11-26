807050e8beffac69a380ad26b0c53129a8daead5
samwilson0745 <sohamdeb0745@gmail.com>
2023-11-26 22:54:21 +0530

    Home modified

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,8 +1,6 @@
 import "./home.css";
 import { TypeAnimation } from 'react-type-animation';
-import soham from '../../../assets/profile/soham-nobg.png';
-import LinkedInLogo from "./Logos/LinkedInLogo";
-import GithubLogo from "./Logos/GithubLogo";
+import Button from "../../components/Button/Button";
 import { Link } from "react-scroll";
 
 const Home = ()=>{
@@ -13,7 +11,7 @@ const Home = ()=>{
     return(
         <div className="home row" name="home" >
             <div style={container1}>
-                <h1 style={{fontSize:'2em',color:'white',fontWeight:"100",letterSpacing:"3px",padding:"0 3px"}}>Welcome !</h1>
+                <h1 style={{fontSize:'2.7em',color:'white',fontWeight:"bold",letterSpacing:"3px",padding:"0 3px"}}>Soham Deb Majumder</h1>
                 <TypeAnimation
                     sequence={[
         // Same substring at the start will only be typed out once, initially
@@ -29,21 +27,19 @@ const Home = ()=>{
                 style={{ fontSize: '4em', display: 'inline-block' ,color:'white',letterSpacing:"3px"}}
                 repeat={Infinity}
             />
-            <h4 style={{color:"white",letterSpacing:"3px",fontWeight:"100",padding:"0 3px"}}>I make the complex simple</h4>
-            <div className="row-icons">
-                <a href="https://www.linkedin.com/in/soham-deb-majumder-35b959210/" className="custom-icon-link">
-                    <LinkedInLogo color="#0077B5"/>
-                </a>
-                <a href="https://github.com/samwilson0745" className="custom-icon-link">
-                    <GithubLogo color="black" />
-                </a>
+            <div className="tag">
+                <h4 style={{color:"white",letterSpacing:"3px",fontWeight:"bold"}}>
+                    Full Stack Developer |
+                    Flutter Developer | 
+                    Django |
+                    Java | 
+                    Python 
+                </h4>
             </div>
+            
             <div>
-                <button class="elevated-button"><Link activeClass="my-active" smooth={true} spy={true} to="contact">Contact me</Link></button>
+                <Button><Link activeClass="my-active" smooth={true} spy={true} to="contact">Contact me</Link></Button>
             </div>
-            </div>
-            <div>
-                <img src={soham} alt="" className="my-image"/>
             </div>
         </div>
     )
