84877b1f8a52d34495a3c8af1189b264ec31f079
samwilson0745 <sohamdeb0745@gmail.com>
2023-11-26 23:21:30 +0530

    loader outline added

---
+++ src/components/Pages/Loading/Loading.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+import './loading.css'; // Import the CSS file for styling
+
+const NameLoading = () => {
+   
+  return (
+    <div className="loading-container">
+      <div className="loading-animation">
+        <div className="letter">S</div>
+        <div className="letter">o</div>
+        <div className="letter">h</div>
+        <div className="letter">a</div>
+        <div className="letter">m</div>
+      </div>
+    </div>
+  );
+};
+export default NameLoading;
